373384a3802e9c1b98d7e8a61f272acd5a18ebaf
masaki1002 <masakioeda1002@yahoo.com>
2019-02-12 13:49:52 +0900

    initial upload test

---
+++ index.js
@@ -0,0 +1,46 @@
+window.addEventListener('DOMContentLoaded', init);
+
+function init() {
+  const width = 960;
+  const height = 540;
+
+  // レンダラーを作成
+  // レンダラーを作成
+  const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#myCanvas')
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(width, height);
+
+  // シーンを作成
+  const scene = new THREE.Scene();
+
+  // カメラを作成
+  const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
+  camera.position.set(0, 0, +1000);
+
+  // 箱を作成
+  const geometry = new THREE.BoxGeometry(500, 500, 500);
+  const material = new THREE.MeshStandardMaterial({color: 0x0000FF});
+  const box = new THREE.Mesh(geometry, material);
+  scene.add(box);
+
+  // 平行光源
+  const directionalLight = new THREE.DirectionalLight(0xFFFFFF);
+  directionalLight.intensity = 2;
+  directionalLight.position.set(1, 1, 1);
+  // シーンに追加
+  scene.add(directionalLight);
+
+  // 初回実行
+  tick();
+
+  function tick() {
+    requestAnimationFrame(tick);
+
+    box.rotation.x += 0.01;
+    box.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+  }
+}
